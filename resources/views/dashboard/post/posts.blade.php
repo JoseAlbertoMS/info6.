f24c097cad7a6bdae925048457669ee2fdf8bace
Bryan Garcia <brusbsbsjlll@gmail.com>
2022-04-19 08:35:42 -0500

    19-04-2022 1

--- resources/views/dashboard/post/posts.blade.php
+++ resources/views/dashboard/post/posts.blade.php
@@ -33,9 +33,7 @@
               <a href="{{ route('post.edit', $post->id) }}" class="btn btn-secondary">Edit</a>
             </td>
             <td> 
-              
               <button data-bs-toggle="modal" data-bs-target="#deleteModal" data-bs-id="{{ $post->id }}" type="submit" class="btn btn-danger">Delete</button>
-            
             </td>
           </tr>
         @endforeach
@@ -44,27 +42,32 @@
 
     <div class="mt-3">{{ $posts->links() }}</div>
 
-<div class="modal fade" id="deleteModal" role="dialog" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="modalLabel"></h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <p>¿Estás seguro de borrar el registro seleccionado?</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <form id="formDelete" action="{{ route('post.destroy', 0) }}" method="POST">
-          @csrf
-          @method("DELETE")
-          <button class="btn btn-danger" type="submit">Delete</button>
-        </form>
+
+    <div class="modal fade" id="deleteModal" role="dialog" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+
+          <div class="modal-header">
+            <h5 class="modal-title" id="modalLabel"></h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+
+          <div class="modal-body">
+            <p>¿Estás seguro de borrar el registro seleccionado?</p>
+          </div>
+
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <form id="formDelete" action="{{ route('post.destroy', 0) }}" method="POST">
+              @csrf
+              @method("DELETE")
+              <button class="btn btn-danger" type="submit">Delete</button>
+            </form>
+          </div>
+
+        </div>
       </div>
     </div>
-  </div>
-</div>
 
 <script>
   var deleteModal = document.getElementById('deleteModal')
@@ -77,10 +80,10 @@
     // and then do the updating in a callback.
     //
     // Update the modal's content.
-    var modalTitle = deleteModal.querySelector('.modal-title')
+    var modalTitle = deleteModal.querySelector('.modal-title');
     var action = formDelete.getAttribute('action').slice(0, -1) + id;
-    //console.log(action)
-    formDelete.setAtribute('action', action)
+    console.log(action)
+    formDelete.action = action;
     modalTitle.textContent = 'Post ' + id
 })
 </script>
