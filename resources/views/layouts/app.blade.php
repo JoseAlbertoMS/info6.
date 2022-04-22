e9ada1e1aae3b17d0c766fc498a84c1e39a57202
bryanbsbs <brusbsbsjlll@gmail.com>
2022-04-21 21:26:38 -0500

    21-04-2022

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,8 +58,13 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+
+                                    @if (Auth::user()->role_id == 1)
+                                        <a class="dropdown-item" href="{{ route('post.index') }}">Administrar</a>
+                                    @endif
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                                       onclick="event.preventDefault()
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
